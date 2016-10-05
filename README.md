90c2f67039105d649ab67f078cc520365d7fdcdf
Florian Topf <florian.topf@gmail.com>
2016-10-05 20:07:29 +0200

    added war support an readme

---
+++ README.md
@@ -0,0 +1,14 @@
+# UrlAnalyzer for ImmobilienScout
+
+Compile:
+```
+./mvnw install
+./mvnw package
+```
+
+Execute WAR file on standalone server:
+```
+java -jar target/urlanalyzer-0.0.1-SNAPSHOT.war
+```
+
+Otherwise the WAR file can be deployed to any capable application server.
--- src/main/java/com/immobilienscout/urlanalyzer/UrlAnalyzerApplication.java
+++ src/main/java/com/immobilienscout/urlanalyzer/UrlAnalyzerApplication.java
@@ -2,9 +2,17 @@ package com.immobilienscout.urlanalyzer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class UrlAnalyzerApplication {
+public class UrlAnalyzerApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(UrlAnalyzerApplication.class);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(UrlAnalyzerApplication.class, args);
     }
